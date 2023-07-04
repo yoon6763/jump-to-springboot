d7730f28c214aed31691dbe374cecf66353e33b0
uknow-mit <yoon6763@naver.com>
2023-07-04 17:43:53 +0900

    Spring Web 의존성 추가

---
+++ src/main/java/com/jumptospringboot/board/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.jumptospringboot.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/sbb")
+    public void index() {
+        System.out.println("index");
+    }
+
+}
