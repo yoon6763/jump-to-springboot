589b55fe650cdc6295fffdd93691bac08d1d239d
uknow-mit <yoon6763@naver.com>
2023-07-13 17:38:44 +0900

    질문 상세 기능

--- src/main/java/com/jumptospringboot/board/service/QuestionService.java
+++ src/main/java/com/jumptospringboot/board/service/QuestionService.java
@@ -1,11 +1,13 @@
 package com.jumptospringboot.board.service;
 
 import com.jumptospringboot.board.entity.Question;
+import com.jumptospringboot.board.exception.DataNotFoundException;
 import com.jumptospringboot.board.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +17,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = questionRepository.findById(id);
+        if(question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
