7016e20b86b8690f03463f431b0ee4f65557a47a
uknow-mit <yoon6763@naver.com>
2023-07-17 15:55:48 +0900

    페이징

--- src/main/java/com/jumptospringboot/board/repository/QuestionRepository.java
+++ src/main/java/com/jumptospringboot/board/repository/QuestionRepository.java
@@ -1,12 +1,19 @@
 package com.jumptospringboot.board.repository;
 
 import com.jumptospringboot.board.entity.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
+
     Question findBySubjectAndContent(String subject, String content);
+
     List<Question> findBySubjectLike(String subject);
+
+    Page<Question> findAll(Pageable pageable);
+
 }
